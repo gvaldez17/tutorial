40176813533ebe6ef87c82b7d55e93cc84c4bd4f
Glenda Valdez <gvaldez@wisc.edu>
2015-06-01 10:53:23 -0500

    .gitignore * added tutorial.Rproj to the gitignore *moved tutorial part one from the old repository into the new repository

---
+++ tutorialprt1.R
@@ -0,0 +1,31 @@
+#tutorial part one
+#experessions
+1 + 1
+"Arr, matey!"
+6 * 7
+#logical values
+3 < 4
+2 + 2 == 5
+T ==TRUE
+#storing a value in a variable
+x <- 42
+x / 2
+x <- "Arr, matey!"
+x <- TRUE
+#sum function
+sum(1, 3, 5)
+#repeat function
+rep("Yo ho!", times = 3)
+#square root function
+sqrt(16)
+#using the help function define use of a certain function
+help(sum)
+#examples for usage of a given function DOES THIS WORK??
+example(min)
+#R commands stored in text files w/an .R extension
+#list files in the following manner
+list.files()
+#To run a script, pass a string with its name to the source function
+source("filename.R")
+#
+
